e28d63d81644bbf21f2f4e44fc88e737e078891f
laurens-in <laurensinauen@gsnail.ch>
2024-04-29 01:16:46 +0200

    add stuff

--- src/bin/pdm.rs
+++ src/bin/pdm.rs
@@ -1,11 +1,11 @@
 #![no_main]
 #![no_std]
 
-use core::{ptr, sync::atomic};
+use core::{ptr, sync::atomic::compiler_fence, sync::atomic::Ordering::SeqCst};
 
-use cortex_m::asm::wfe;
+use cortex_m;
 use nrf52840_hal::{
-    gpio::{p0, p1, Floating, Input, Level, Output, Pin, PushPull},
+    gpio::{p0, p1, DriveConfig, Floating, Input, Level, OpenDrainConfig, Output, Pin, PushPull},
     pac::{self, pdm::SAMPLE},
 };
 use pdm_bare_metal as _; // global logger + panicking-behavior + memory layout
@@ -13,26 +13,31 @@ use pdm_bare_metal as _; // global logger + panicking-behavior + memory layout
 const SAMPLE_RATE: u32 = 16000; // Sample rate in Hz
 const TIMER_FREQUENCY: u32 = 1_032_000; // Timer frequency in Hz
 const BUFFER_LENGTH: usize = (TIMER_FREQUENCY / SAMPLE_RATE) as usize;
+// const BUFFER_LENGTH: usize = 1_000;
+
+static DATA: [u16; BUFFER_LENGTH] = [0; BUFFER_LENGTH];
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
+    defmt::println!("DATA: {}", ptr::addr_of!(DATA));
+    let mut data_ptr: u32 = 0x20000000;
+    data_ptr += ptr::addr_of!(DATA) as u32;
+    // let data: [u16; BUFFER_LENGTH] = [0; BUFFER_LENGTH];
+
+    defmt::println!("pointer: {}", data_ptr);
+
     let dp = pac::Peripherals::take().unwrap();
 
     let port0 = p0::Parts::new(dp.P0);
     let port1 = p1::Parts::new(dp.P1);
 
-    static DUMMY: [u16; 1] = [0; 1];
+    port1
+        .p1_09
+        .into_push_pull_output_drive(Level::Low, DriveConfig::Standard0Standard1);
 
-    // defmt::println!("initialized data: {}", data);
-
-    // intialize PDM
-    let _clk: Pin<Output<PushPull>> = port1.p1_09.into_push_pull_output(Level::Low).degrade();
-    let _dat: Pin<Input<Floating>> = port0.p0_08.into_floating_input().degrade();
+    port0.p0_08.into_floating_input();
 
     let pdm = dp.PDM;
-
-    // enable PDM peripheral
-    pdm.enable.write(|w| w.enable().bit(true));
 
     // configure PDM clock
     pdm.psel
@@ -46,9 +51,9 @@ fn main() -> ! {
 
     pdm.pdmclkctrl.write(|w| w.freq().default());
 
-    // set mode to mono
+    // set mode to stereo
     pdm.mode
-        .write(|w| w.operation().bit(true).edge().left_rising());
+        .write(|w| w.operation().bit(false).edge().left_rising());
 
     // set gain
     pdm.gainl.write(|w| unsafe { w.gainl().bits(0x28) });
@@ -58,61 +63,62 @@ fn main() -> ! {
 
     pdm.ratio.write(|w| w.ratio().bit(false));
 
-    // pdm.sample
-    //     .ptr
-    //     .write(|w| unsafe { w.sampleptr().bits(data.as_ptr() as u32)});
+    compiler_fence(SeqCst);
 
-    // pdm.sample
-    //     .ptr
-    //     .write(|w| unsafe { w.sampleptr().bits(data.as_ptr() as u32) });
+    // enable PDM peripheral
+    pdm.enable.write(|w| w.enable().bit(true));
 
-    // pdm.sample
-    //     .maxcnt
-    //     .write(|w| unsafe { w.buffsize().bits((BUFFER_LENGTH).try_into().unwrap()) });
+    compiler_fence(SeqCst);
+
+    pdm.sample
+        .ptr
+        .write(|w| unsafe { w.sampleptr().bits(data_ptr as u32) });
+
+    pdm.sample
+        .maxcnt
+        .write(|w| unsafe { w.buffsize().bits((BUFFER_LENGTH).try_into().unwrap()) });
 
     pdm.tasks_start.write(|w| w.tasks_start().bit(true));
 
     loop {
-        let data: [u16; BUFFER_LENGTH] = [0; BUFFER_LENGTH];
-
         pdm.sample
             .ptr
-            .write(|w| unsafe { w.sampleptr().bits(data.as_ptr() as u32) });
+            .write(|w| unsafe { w.sampleptr().bits(data_ptr as u32) });
 
         pdm.sample
             .maxcnt
             .write(|w| unsafe { w.buffsize().bits((BUFFER_LENGTH).try_into().unwrap()) });
 
-        // defmt::println!("started bit: {}", pdm.events_started.read().bits());
-
-        // defmt::println!("data at: {}", data.as_ptr());
-
-        // while pdm.events_started.read().bits() == 0 {
-        //     defmt::trace!("waiting for start...")
-        // }
-
-        // cortex_m::asm::delay(50_000);
+        compiler_fence(SeqCst);
 
         pdm.events_end.write(|w| w.events_end().bit(false));
-        defmt::trace! {"clear end bit"};
+        defmt::println! {"clear end bit"};
 
-        // cortex_m::asm::delay(50_000);
-
-        // pdm.sample
-        //     .ptr
-        //     .write(|w| unsafe { w.sampleptr().bits(DUMMY.as_ptr() as u32) });
-
-        // pdm.sample.maxcnt.write(|w| unsafe { w.buffsize().bits(1) });
+        compiler_fence(SeqCst);
 
         while pdm.events_end.read().bits() == 0 {
-            // defmt::trace!("waiting for end");
-            // cortex_m::asm::delay(50_000);
+            cortex_m::asm::delay(100);
         }
 
-        // defmt::trace!("end received!");
+        compiler_fence(SeqCst);
 
-        defmt::println!("data {}: {}", data.as_ptr(), data);
-        defmt::println!("result {}", pdm.sample.ptr.read().bits())
+        unsafe {
+            defmt::println!(
+                "data {:x}: {}",
+                data_ptr,
+                ptr::read_volatile(data_ptr as *const [u16; BUFFER_LENGTH])
+            );
+        }
+
+        defmt::println!("data: {}", data_ptr);
+
+        defmt::println!(
+            "address reg {:x}, address var: {:x}",
+            pdm.sample.ptr.read().bits(),
+            data_ptr
+        );
+
+        compiler_fence(SeqCst);
     }
 
     // pdm_bare_metal::exit()
